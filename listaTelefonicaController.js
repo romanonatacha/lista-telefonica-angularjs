17c544a030485d90dcac57bcf133ce8eae5b4f9a
Natacha Romano <romano.n@hotmail.com>
2018-07-08 22:52:05 -0300

    implementação apagar contatos

--- listaTelefonicaController.js
+++ listaTelefonicaController.js
@@ -15,9 +15,53 @@ angular.module('listaTelefonica').controller('listaTelefonicaCtrl', function ($s
             telefone: '999996666'
         }
     ];
+    $scope.operadoras = [
+        {
+            nome: 'Oi',
+            codigo: 14,
+            categoria: 'Celular'
+        },
+        {
+            nome: 'Vivo',
+            codigo: 15,
+            categoria: 'Celular'
+        },
+        {
+            nome: 'Tim',
+            codigo: 41,
+            categoria: 'Celular'
+        },
+        {
+            nome: 'Claro',
+            codigo: 21,
+            categoria: 'Celular'
+        },
+        {
+            nome: 'GVT',
+            codigo: 25,
+            categoria: 'Fixo'
+        },
+        {
+            nome: 'Embratel',
+            codigo: 21,
+            categoria: 'Fixo'
+        }
+    ];
     $scope.adicionarContato = function (contato) {
         $scope.contatos.push(angular.copy(contato));
         delete $scope.contato;
+    };
+
+    $scope.apagarContatos = function (contatos) {
+        $scope.contatos = contatos.filter(function (contato) {
+            if (!contato.selecionado) return contato;
+        });
+    };
+
+    $scope.isContatoSelecionado = function (contatos) {
+        return contatos.some(function (contato) {
+            return contato.selecionado;
+        });
     };
 
 });
